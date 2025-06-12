595ff5c4ba3986ccb7038b69f5d24bfc471eb112
Alaminplabon <donplabon94@gmail.com>
2025-06-12 09:48:46 +0600

    withdraw

--- src/app/modules/withdraw/withdraw.route.ts
+++ src/app/modules/withdraw/withdraw.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { withdrawController } from './withdraw.controller';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../user/user.constants';
 
 const router = Router();
 
-router.post('/create-withdraw', withdrawController.createwithdraw);
+router.post(
+  '/create-withdraw',
+  auth(USER_ROLE.driver),
+  withdrawController.createwithdraw,
+);
 router.patch('/update/:id', withdrawController.updatewithdraw);
 router.delete('/:id', withdrawController.deletewithdraw);
 
