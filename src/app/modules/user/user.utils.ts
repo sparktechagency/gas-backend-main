56f321acc3ae1399c9d3c8cf3f21f6c38742a68a
Alaminplabon <donplabon94@gmail.com>
2025-06-19 15:29:31 +0600

    remove corn

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,21 +1,21 @@
-import cron from 'node-cron';
-import { User } from './user.models';
+// import cron from 'node-cron';
+// import { User } from './user.models';
 
-// Runs every day at midnight
-cron.schedule('*/5 * * * * *', async () => {
-  try {
-    const users = await User.find({ durationDay: { $gt: 0 } });
+// // Runs every day at midnight
+// cron.schedule('*/5 * * * * *', async () => {
+//   try {
+//     const users = await User.find({ durationDay: { $gt: 0 } });
 
-    for (const user of users) {
-      const remeningDurationDay = user.durationDay - 1;
-      await User.updateOne(
-        { _id: user._id },
-        { $set: { remeningDurationDay } },
-      );
-    }
+//     for (const user of users) {
+//       const remeningDurationDay = user.durationDay - 1;
+//       await User.updateOne(
+//         { _id: user._id },
+//         { $set: { remeningDurationDay } },
+//       );
+//     }
 
-    console.log('User durations updated.');
-  } catch (error) {
-    console.error('Error running user duration cron:', error);
-  }
-});
+//     console.log('User durations updated.');
+//   } catch (error) {
+//     console.error('Error running user duration cron:', error);
+//   }
+// });
