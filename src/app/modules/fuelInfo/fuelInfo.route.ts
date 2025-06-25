4012c02431d6d3841a5f2805b65225f388d88e8a
Alaminplabon <donplabon94@gmail.com>
2025-06-25 15:26:05 +0600

    roure

--- src/app/modules/fuelInfo/fuelInfo.route.ts
+++ src/app/modules/fuelInfo/fuelInfo.route.ts
@@ -7,14 +7,15 @@ const router = Router();
 
 router.post('/create', fuelInfoController.createfuelInfo);
 router.patch('/update/:id', fuelInfoController.updatefuelInfo);
-router.delete('/:id', fuelInfoController.deletefuelInfo);
-router.get('/', auth(USER_ROLE.user), fuelInfoController.getAllfuelInfo);
-router.get('/', auth(USER_ROLE.user), fuelInfoController.getAllfuelInfo);
 router.get(
   '/userzipprise',
   auth(USER_ROLE.user),
   fuelInfoController.getAllfuelInfoByZip,
 );
+router.delete('/:id', fuelInfoController.deletefuelInfo);
+router.get('/', auth(USER_ROLE.user), fuelInfoController.getAllfuelInfo);
+router.get('/', auth(USER_ROLE.user), fuelInfoController.getAllfuelInfo);
+
 // Fix: Use correct handlers
 router.get('/:id', fuelInfoController.getfuelInfoById);
 router.get('/', fuelInfoController.getAllfuelInfo);
